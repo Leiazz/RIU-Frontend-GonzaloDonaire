688dab6400d2dab91030809f194c6bb1232379bf
Leeiazz <leeiaz7554@gmail.com>
2025-07-24 10:01:50 -0300

    add HeroesService and related files, define Hero interface and HEROES data

---
+++ src/app/heroes/data/heroesdb.ts
@@ -0,0 +1,24 @@
+import { Hero } from '../interfaces/hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Superman', power: 'Super strength', universe: 'DC' },
+  { id: 2, name: 'Spider-Man', power: 'Spider-sense', universe: 'Marvel' },
+  { id: 3, name: 'Wonder Woman', power: 'Super strength', universe: 'DC' },
+  {
+    id: 4,
+    name: 'Iron Man',
+    power: 'Genius-level intellect',
+    universe: 'Marvel',
+  },
+  { id: 5, name: 'Batman', power: 'Peak human condition', universe: 'DC' },
+  { id: 6, name: 'Hulk', power: 'Super strength', universe: 'Marvel' },
+  { id: 7, name: 'Flash', power: 'Super speed', universe: 'DC' },
+  { id: 8, name: 'Thor', power: 'God of Thunder', universe: 'Marvel' },
+  { id: 9, name: 'Green Lantern', power: 'Power ring', universe: 'DC' },
+  {
+    id: 10,
+    name: 'Captain America',
+    power: 'Super soldier serum',
+    universe: 'Marvel',
+  },
+];
