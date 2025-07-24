a9545a71c25c774809ec5a142e2d239af76513fe
Leeiazz <leeiaz7554@gmail.com>
2025-07-24 10:57:39 -0300

    getHeroes and getHeroById added

--- src/app/heroes/services/heroes-service.ts
+++ src/app/heroes/services/heroes-service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Hero } from '../interfaces/hero';
+import { HEROES } from '../data/heroesdb';
+import { HeroesMapper } from '../mappers/HeroesMapper';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroesService {
-  
+  private _heroes = signal<Hero[]>([]);
+  loading = signal<boolean>(false);
+
+  getHeroes() {
+    this.loading.set(true);
+    setTimeout(() => {
+      const heroesFromStorage = JSON.parse(
+        localStorage.getItem('heroes') || '[]'
+      );
+      if (heroesFromStorage.length > 0) {
+        const mappedHeroes = HeroesMapper(heroesFromStorage);
+        this._heroes.set(mappedHeroes);
+      } else {
+        localStorage.setItem('heroes', JSON.stringify(HEROES));
+        this._heroes.set(HEROES);
+      }
+      this.loading.set(false);
+    }, 1000);
+  }
+
+  getHeroById(id: number): Hero | undefined {
+    return this._heroes().find((hero) => hero.id === id);
+  }
 }
