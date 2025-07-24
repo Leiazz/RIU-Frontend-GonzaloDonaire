1064ca6b734342c11e4e370911986013f317f9f3
Leeiazz <leeiaz7554@gmail.com>
2025-07-24 11:10:31 -0300

    add routing for HeroesPage and HeroesFormPage components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HeroesPage } from './heroes/pages/heroes-page/heroes-page';
+import { HeroesFormPage } from './heroes/pages/heroes-form-page/heroes-form-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HeroesPage },
+  { path: 'form', component: HeroesFormPage },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
