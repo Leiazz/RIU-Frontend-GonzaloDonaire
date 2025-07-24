a2ed8db3709701d7eac0b7ae5dce28457be96e12
Leeiazz <leeiaz7554@gmail.com>
2025-07-24 17:21:50 -0300

    add HeroesFormPage component with form and styles

--- src/app/heroes/pages/heroes-form-page/heroes-form-page.ts
+++ src/app/heroes/pages/heroes-form-page/heroes-form-page.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroes-form-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
   templateUrl: './heroes-form-page.html',
-  styleUrl: './heroes-form-page.css'
+  styleUrl: './heroes-form-page.css',
 })
 export class HeroesFormPage {
+  form: FormGroup = new FormGroup({
+    id: new FormControl<number>(0),
+    name: new FormControl<string>(''),
+    power: new FormControl<string>(''),
+    universe: new FormControl<string>(''),
+  });
 
+  constructor(activatedRoute: ActivatedRoute) {
+    const heroId = activatedRoute.snapshot.queryParamMap.get('id');
+    if (heroId) {
+      console.log({ heroId });
+    }
+  }
 }
