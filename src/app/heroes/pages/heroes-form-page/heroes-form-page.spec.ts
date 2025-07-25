a5b8434f9c10e58f9fe24b0ca1ebfe035760a3f3
Leeiazz <leeiaz7554@gmail.com>
2025-07-25 09:43:44 -0300

    testing: add HeroesFormPage testing

--- src/app/heroes/pages/heroes-form-page/heroes-form-page.spec.ts
+++ src/app/heroes/pages/heroes-form-page/heroes-form-page.spec.ts
@@ -1,16 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeroesFormPage } from './heroes-form-page';
+import { HeroesService } from '../../services/heroes-service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('HeroesFormPage', () => {
   let component: HeroesFormPage;
   let fixture: ComponentFixture<HeroesFormPage>;
+  let mockHeroesService: any;
+  let mockRouter: jasmine.SpyObj<Router>;
+  let mockActivatedRoute: any;
 
   beforeEach(async () => {
+    mockHeroesService = {
+      getHeroById: jasmine.createSpy('getHeroById'),
+      createHero: jasmine.createSpy('createHero'),
+      updateHero: jasmine.createSpy('updateHero'),
+      loading: () => false,
+      loadingDelete: () => false,
+      loadingCreateOrEdit: () => false,
+    } as any;
+    mockRouter = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    mockActivatedRoute = { queryParamMap: of(new Map()) };
+
     await TestBed.configureTestingModule({
-      imports: [HeroesFormPage]
-    })
-    .compileComponents();
+      imports: [HeroesFormPage],
+      providers: [
+        { provide: HeroesService, useValue: mockHeroesService },
+        { provide: Router, useValue: mockRouter },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeroesFormPage);
     component = fixture.componentInstance;
@@ -19,5 +39,66 @@ describe('HeroesFormPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set mode to create if no id param', () => {
+    component.mode.set('edit');
+    spyOn(component, 'idHero').and.returnValue(null);
+    component.populateForm();
+    expect(component.mode()).toBe('create');
+  });
+
+  it('should set mode to edit and patch form if id param and hero found', () => {
+    const hero = { id: 1, name: 'A', power: 'X', universe: 'U' };
+    mockHeroesService.getHeroById.and.returnValue(hero);
+    spyOn(component, 'idHero').and.returnValue('1');
+    const patchSpy = spyOn(component.form, 'patchValue');
+    component.populateForm();
+    expect(component.mode()).toBe('edit');
+    expect(patchSpy).toHaveBeenCalledWith(hero);
+  });
+
+  it('should set mode to edit and not patch form if id param and hero not found', () => {
+    mockHeroesService.getHeroById.and.returnValue(undefined);
+    spyOn(component, 'idHero').and.returnValue('1');
+    const patchSpy = spyOn(component.form, 'patchValue');
+    component.populateForm();
+    expect(component.mode()).toBe('edit');
+    expect(patchSpy).not.toHaveBeenCalled();
+  });
+
+  it('should call createHero on save in create mode', () => {
+    component.mode.set('create');
+    component.form.setValue({ id: 0, name: 'A', power: 'X', universe: 'U' });
+    spyOnProperty(component.form, 'invalid', 'get').and.returnValue(false);
+    component.onSaveHero();
+    expect(mockHeroesService.createHero).toHaveBeenCalledWith(
+      component.form.value
+    );
+  });
+
+  it('should call updateHero on save in edit mode', () => {
+    component.mode.set('edit');
+    component.form.setValue({ id: 1, name: 'A', power: 'X', universe: 'U' });
+    spyOnProperty(component.form, 'invalid', 'get').and.returnValue(false);
+    component.onSaveHero();
+    expect(mockHeroesService.updateHero).toHaveBeenCalledWith(
+      component.form.value
+    );
+  });
+
+  it('should not call createHero or updateHero if form is invalid', () => {
+    spyOnProperty(component.form, 'invalid', 'get').and.returnValue(true);
+    component.mode.set('create');
+    component.onSaveHero();
+    component.mode.set('edit');
+    component.onSaveHero();
+    expect(mockHeroesService.createHero).not.toHaveBeenCalled();
+    expect(mockHeroesService.updateHero).not.toHaveBeenCalled();
+  });
+
+  it('should navigate to / on cancel', () => {
+    component.onCancel();
+    expect(mockRouter.navigateByUrl).toHaveBeenCalledWith('/');
   });
 });
