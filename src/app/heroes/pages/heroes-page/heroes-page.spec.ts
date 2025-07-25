5be4f60ba2d4ee04c2f499b6792760d8f954b828
Leeiazz <leeiaz7554@gmail.com>
2025-07-25 09:43:08 -0300

    testing: add heroes page testing

--- src/app/heroes/pages/heroes-page/heroes-page.spec.ts
+++ src/app/heroes/pages/heroes-page/heroes-page.spec.ts
@@ -1,16 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeroesPage } from './heroes-page';
+import { MatDialog } from '@angular/material/dialog';
+import { HeroesService } from '../../services/heroes-service';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { Hero } from '../../interfaces/hero';
+import { ConfirmDelete } from '../../components/confirm-delete/confirm-delete';
 
 describe('HeroesPage', () => {
   let component: HeroesPage;
   let fixture: ComponentFixture<HeroesPage>;
 
+  let mockDialog: jasmine.SpyObj<MatDialog>;
+  let mockHeroesService: jasmine.SpyObj<HeroesService>;
+  let mockRouter: jasmine.SpyObj<Router>;
+  const hero: Hero = {
+    id: 1,
+    name: 'Test Hero',
+    power: 'test',
+    universe: 'Marvel',
+  };
+
   beforeEach(async () => {
+    mockDialog = jasmine.createSpyObj('MatDialog', ['open']);
+    mockHeroesService = {
+      deleteHero: jasmine.createSpy('deleteHero'),
+      loading: () => false,
+      loadingDelete: () => false,
+      loadingCreateOrEdit: () => false,
+      heroesFiltered: () => [],
+    } as any;
+    mockRouter = jasmine.createSpyObj('Router', ['navigateByUrl']);
     await TestBed.configureTestingModule({
-      imports: [HeroesPage]
-    })
-    .compileComponents();
+      imports: [HeroesPage],
+      providers: [
+        { provide: MatDialog, useValue: mockDialog },
+        { provide: HeroesService, useValue: mockHeroesService },
+        { provide: Router, useValue: mockRouter },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeroesPage);
     component = fixture.componentInstance;
@@ -19,5 +47,42 @@ describe('HeroesPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to form on edit', () => {
+    component.onEditButton(hero);
+    expect(mockRouter.navigateByUrl).toHaveBeenCalledWith(
+      `/form?id=${hero.id}`
+    );
+  });
+
+  it('should open dialog and call deleteHero if confirmed', () => {
+    const afterClosed$ = of(true);
+    mockDialog.open.and.returnValue({ afterClosed: () => afterClosed$ } as any);
+    component.onDeleteButton(hero);
+    expect(mockDialog.open).toHaveBeenCalledWith(ConfirmDelete, {
+      data: { hero },
+    });
+    expect(mockHeroesService.deleteHero).toHaveBeenCalledWith(hero.id);
+  });
+
+  it('should open dialog and NOT call deleteHero if not confirmed', () => {
+    const afterClosed$ = of(false);
+    mockDialog.open.and.returnValue({ afterClosed: () => afterClosed$ } as any);
+    component.onDeleteButton(hero);
+    expect(mockDialog.open).toHaveBeenCalledWith(ConfirmDelete, {
+      data: { hero },
+    });
+    expect(mockHeroesService.deleteHero).not.toHaveBeenCalled();
+  });
+
+  it('should open dialog and NOT call deleteHero if result is undefined', () => {
+    const afterClosed$ = of(undefined);
+    mockDialog.open.and.returnValue({ afterClosed: () => afterClosed$ } as any);
+    component.onDeleteButton(hero);
+    expect(mockDialog.open).toHaveBeenCalledWith(ConfirmDelete, {
+      data: { hero },
+    });
+    expect(mockHeroesService.deleteHero).not.toHaveBeenCalled();
   });
 });
